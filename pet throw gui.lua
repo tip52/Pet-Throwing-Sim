d09379e57051843814a117119a863626d60708d4
tip <103219982+tip52@users.noreply.github.com>
2023-07-22 20:54:24 -0600

    Update pet throw gui.lua
    added some stuff

--- pet throw gui.lua
+++ pet throw gui.lua
@@ -1,6 +1,27 @@
 getgenv().autoOrb = false
 getgenv().autoLaunch = false
 getgenv().autoView = false
+-- hatching
+getgenv().triple = false
+getgenv().octuple = false
+getgenv().hatch = false
+getgenv().EggName = nil
+getgenv().tpToEgg = false
+local newName
+for i,v in pairs(game.CoreGui:GetChildren()) do
+    if v:FindFirstChild("Main") and v.Main:FindFirstChild("MainHeader") and v.Main:FindFirstChild("pages") then
+        v:Destroy()
+    end
+end
+
+task.spawn(function()
+    local VirtualUser = game:service 'VirtualUser'
+    game.Players.LocalPlayer.Idled:connect(function()
+        VirtualUser:CaptureController()
+        VirtualUser:ClickButton2(Vector2.new())
+    end)
+end)
+
 
 local Network = require(game:GetService("ReplicatedStorage").Library.Client.Network)
 local Fire, Invoke = Network.Fire, Network.Invoke
@@ -9,31 +30,61 @@ local FireHook = hookfunction(debug.getupvalue(Fire, 1), function(...) return tr
 local orbs = workspace:WaitForChild("__MAP"):WaitForChild("Interactive"):WaitForChild("Orbs")
 
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
-local Window = Library.CreateLib("Yeet a Pet by Hellsend", "Ocean")
+local Window = Library.CreateLib("Yeet a Pet by Tip", "Ocean")
+local News = Window:NewTab("News")
 local Tab = Window:NewTab("Main")
+local Eggs = Window:NewTab("Eggs")
 local Section = Tab:NewSection("Farming")
 Section:NewToggle("Auto Orb", "Auto collects orbs", function(v)
     getgenv().autoOrb = v
-    print(v)
+
 end)
 Section:NewToggle("Auto Launch", "Automatically launches for you", function(v)
     getgenv().autoLaunch = v
-    print(v)
+
 end)
 
 local Section = Tab:NewSection("Visual")
-Section:NewButton("View character", "Views your character", function()
-    local camera = workspace.CurrentCamera
-    local plr = game.Players.LocalPlayer
-    local chr = plr.Character
-    local hum = chr:WaitForChild("Humanoid")
-    camera.CameraSubject = hum
-end)
-Section:NewToggle("Auto view character", "Stops camera from changing subjects", function(v)
+Section:NewToggle("View character", "Views character when on", function(v)
     getgenv().autoView = v
 end)
 
+local NewsSection = News:NewSection("News")
+NewsSection:NewLabel("- Made view character better")
+NewsSection:NewLabel("- Added anti afk")
+NewsSection:NewLabel("- Added auto open egg stuff")
+NewsSection:NewLabel("- Now deletes previous uis when executing")
+NewsSection:NewLabel("Script by Tip, enjoy ;)")
 
+local EggSec = Eggs:NewSection("Hatching")
+
+EggSec:NewToggle("Start Hatching", "Auto hatches", function(v)
+    getgenv().hatch = v
+end)
+
+EggSec:NewToggle("Tp to egg", "Teleports to egg", function(v)
+    getgenv().tpToEgg = v
+end)
+
+EggSec:NewDropdown("Eggs", "Eggs", {"Jetpack Egg", "Fireball Egg", "Wild Egg"}, function(v)
+    getgenv().EggName = v
+end)
+
+EggSec:NewDropdown("Gold Eggs", "Gold Eggs", {"Golden Jetpack Egg", "Golden Fireball Egg", "Golden Wild Egg"}, function(v)
+    getgenv().EggName = v
+end)
+
+EggSec:NewDropdown("Hatch Mode", "DropdownInf", {"Single", "Triple", "Octuple"}, function(v)
+    if v == "Single" then
+        getgenv().triple,getgenv().octuple = false,false
+    elseif v == "Triple" then
+        getgenv().triple = true
+        getgenv().octuple = false
+    elseif v == "Octuple" then
+        getgenv().octuple = true
+        getgenv().triple = false
+    end
+end)
 
 task.spawn(function()
     while task.wait() do -- loop
@@ -70,6 +121,34 @@ task.spawn(function()
             if camera.CameraSubject ~= hum then
                 camera.CameraSubject = hum
             end
+    else
+        if workspace:WaitForChild("__THINGS"):WaitForChild("Yeet"):FindFirstChild(game.Players.LocalPlayer.Name .."_CLIENT") then
+            workspace.CurrentCamera.CameraSubject = workspace:WaitForChild("__THINGS"):WaitForChild("Yeet"):FindFirstChild(game.Players.LocalPlayer.Name .."_CLIENT")
+        end
+    end
+    end
+end)
+
+task.spawn(function()
+    while task.wait() do -- loop
+        if getgenv().hatch and getgenv().EggName ~= nil then
+            --#region egg tp
+            if getgenv().tpToEgg then
+                local EggsFolder = workspace:WaitForChild("__MAP"):FindFirstChild("Eggs")
+                newName = getgenv().EggName
+                if string.find(getgenv().EggName,"Golden ") then
+                    newName = string.gsub(getgenv().EggName,"Golden ","")
+                end
+                for i,v in pairs(EggsFolder:GetChildren()) do
+                    for i,v in pairs(v:GetChildren()) do
+                        if v.Name == "PLATFORM" and v:FindFirstChild("SectionName") and v.SectionName:FindFirstChild("SurfaceGui") and v.SectionName.SurfaceGui:FindFirstChild("TextLabel") and string.find(v.SectionName.SurfaceGui.TextLabel.Text,newName) then
+                            game.Players.LocalPlayer.Character:WaitForChild("HumanoidRootPart").CFrame = v.SectionName.CFrame * CFrame.new(0,3,0)
+                        end
+                    end
+                end
+                --#endregion
+            end
+            Invoke("Buy Egg",getgenv().EggName,getgenv().triple,getgenv().octuple)
     end
     end
 end)
